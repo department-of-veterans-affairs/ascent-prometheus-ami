7f08763ef3cf67b9e07467478611e64d9398d9c6
jasonluck <jluck@meetveracity.com>
2018-07-27 09:48:32 -0400

    Outputing private Ip address of monitor server

--- modules/monitor-server/outputs.tf
+++ modules/monitor-server/outputs.tf
@@ -7,3 +7,6 @@ output "base_monitor_security_group_id" {
   value = "${aws_security_group.base_monitor_inbound_security_group.id}"
 }
 
+output "private_ip" {
+  value = "${aws_instance.monitor.private_ip}"
+}
